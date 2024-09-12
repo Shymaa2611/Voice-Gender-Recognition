8849c1045bcce0d6afeccf47f06808df9f92f17b
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-12 17:12:03 +0300

    first commit

--- model.py
+++ model.py
@@ -1,30 +1,19 @@
-import torch
 import torch.nn as nn
+
 class SGR(nn.Module):
     def __init__(self, input_dim):
         super(SGR, self).__init__()
         self.model = nn.Sequential(
             nn.BatchNorm1d(input_dim),
-            nn.Linear(input_dim, 64),  # Reduced from 128 to 64
+            nn.Linear(input_dim, 100),
             nn.ReLU(),
             nn.Dropout(0.5),
-
-            nn.Linear(64, 128),
+            nn.Linear(100, 200),
             nn.ReLU(),
             nn.Dropout(0.5),
-            
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            
-            nn.Linear(64, 1),
+            nn.Linear(200, 1),
             nn.Sigmoid()
         )
+
+    def forward(self, X):
+        return self.model(X)
