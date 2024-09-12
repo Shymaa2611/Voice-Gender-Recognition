e003a11cc54e2c462df92e4c806ee4dfa360ce80
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-12 03:21:26 +0300

    first commit

--- dataset.py
+++ dataset.py
@@ -1,11 +1,11 @@
-from datasets import load_dataset
-from args import *
 import torch
-from datasets import  Dataset
-import numpy as np
+from datasets import load_dataset
 from torch.utils.data import Dataset, DataLoader
 from transformers import Wav2Vec2Processor, Wav2Vec2Model
+import numpy as np
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def load_data():
     dataset = load_dataset("7wolf/gender-balanced-10k-voice-samples")
     return dataset
@@ -13,10 +13,7 @@ def load_data():
 def extract_wav2vec_features(batch):
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
     model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base").to(device)
-
     audio_dicts = batch['audio']  
-    processor = processor
-    model=model
     features = []
     for audio_dict in audio_dicts:
         if isinstance(audio_dict, dict):
@@ -37,21 +34,24 @@ def extract_wav2vec_features(batch):
     return {'features': np.array(features)}
 
 def apply_change():
-    dataset=load_data()
+    dataset = load_data()
     dataset = dataset.map(extract_wav2vec_features, batched=True)
     label_list = sorted(set(dataset['train']['label']))
     label_to_id = {label: idx for idx, label in enumerate(label_list)}
     return label_to_id
 
 def label_to_int(batch):
-    label_to_id=apply_change()
-    batch['label'] = [label_to_id[label] for label in batch['label']]
+    label_to_id = apply_change()
+    if isinstance(batch['label'], list):
+        batch['label'] = [label_to_id[label] for label in batch['label']]
+    else:
+        batch['label'] = [label_to_id.get(batch['label'], -1)]
     return batch
 
 def final_dataset():
-     dataset=load_data()
-     dataset = dataset.map(label_to_int)
-     return dataset
+    dataset = load_data()
+    dataset = dataset.map(label_to_int, batched=True)
+    return dataset
 
 class AudioDataset(Dataset):
     def __init__(self, data):
@@ -64,12 +64,12 @@ class AudioDataset(Dataset):
     def __getitem__(self, idx):
         return {'features': self.features[idx].to(device),
                 'label': self.labels[idx].to(device)}
-    
+
 def get_loaders():
-    dataset=final_dataset()
+    dataset = final_dataset()
     train_data = AudioDataset(dataset['train'])
     test_data = AudioDataset(dataset['test'])
     train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=32)
-    dim=54614
-    return dim,train_loader,test_loader
+    dim = train_data.features.shape[1]  
+    return dim, train_loader, test_loader
