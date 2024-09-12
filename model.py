1cbd5702cf0c178d15ff009bdf98ecbe328ca733
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-09-12 18:36:19 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ class SGR(nn.Module):
     def __init__(self, input_dim):
         super(SGR, self).__init__()
         self.model = nn.Sequential(
+            nn.BatchNorm1d(input_dim),
             nn.Linear(input_dim, 100),
-            nn.BatchNorm1d(100),  # Batch normalization applied after first linear layer
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(100, 200),
@@ -16,6 +16,4 @@ class SGR(nn.Module):
         )
 
     def forward(self, X):
-        # Flatten the input to match the expected input dimension
-        X = X.view(X.size(0), -1)  # Flatten (batch_size, time_steps, n_mfcc) to (batch_size, input_dim)
         return self.model(X)
